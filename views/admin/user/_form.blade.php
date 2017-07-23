3f4adc66ee6f94d26f9fb14eaa7c1aa4b507dfc4
abr4xas <me@abr4xas.org>
2017-07-23 19:27:11 -0400

    fix en vistas admin usuario :bug:

--- views/admin/user/_form.blade.php
+++ views/admin/user/_form.blade.php
@@ -1,14 +1,14 @@
 <!-- Name Form Input -->
 <div class="form-group @if ($errors->has('name')) has-error @endif">
     {!! Form::label('name', 'Name') !!}
-    {!! Form::text('name', null, ['class' => 'form-control', 'placeholder' => 'Name']) !!}
+    {!! Form::text('name', null, ['class' => 'form-control', 'placeholder' => 'Name', 'readonly']) !!}
     @if ($errors->has('name')) <p class="help-block">{{ $errors->first('name') }}</p> @endif
 </div>
 
 <!-- email Form Input -->
 <div class="form-group @if ($errors->has('email')) has-error @endif">
     {!! Form::label('email', 'Email') !!}
-    {!! Form::text('email', null, ['class' => 'form-control', 'placeholder' => 'Email']) !!}
+    {!! Form::text('email', null, ['class' => 'form-control', 'placeholder' => 'Email', 'readonly']) !!}
     @if ($errors->has('email')) <p class="help-block">{{ $errors->first('email') }}</p> @endif
 </div>
 
