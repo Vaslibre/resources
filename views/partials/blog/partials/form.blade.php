5e7b14217bf6a6b8d7a044176cb16f8f0312bb77
angel <me@abr4xas.org>
2017-08-06 19:13:28 -0400

    fix :bug:

--- views/partials/blog/partials/form.blade.php
+++ views/partials/blog/partials/form.blade.php
@@ -1,12 +1,12 @@
 <div class="form-group @if ($errors->has('title')) has-error @endif">
-    {!! Form::text('titulo', $blog->title, ['class' => 'form-control', 'placeholder' => 'Titulo de la publicación']) !!}
+    {!! Form::text('titulo', $notas->title, ['class' => 'form-control', 'placeholder' => 'Titulo de la publicación']) !!}
     @if ($errors->has('title')) <p class="text-danger small">{{ $errors->first('title') }}</p> @endif
 </div>
 <div class="form-group @if ($errors->has('description')) has-error @endif">
-    {!! Form::text('description', $blog->description, ['class' => 'form-control', 'placeholder' => 'Intro de la publicación']) !!}
+    {!! Form::text('description', $notas->description, ['class' => 'form-control', 'placeholder' => 'Intro de la publicación']) !!}
     @if ($errors->has('description')) <p class="text-danger small">{{ $errors->first('description') }}</p> @endif
 </div>
 <div class="form-group @if ($errors->has('content')) has-error @endif">
-    {!! Form::textarea('content', $blog->content, ['class' => 'form-control', 'placeholder' => 'Intro de la publicación', 'id' => 'summernote']) !!}
+    {!! Form::textarea('content', $notas->content, ['class' => 'form-control', 'placeholder' => 'Intro de la publicación', 'id' => 'summernote']) !!}
     @if ($errors->has('content')) <p class="text-danger small">{{ $errors->first('content') }}</p> @endif
 </div>
