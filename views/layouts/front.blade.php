b7284211058c2d79b74eaa3187604a7d42b78d24
angel <me@abr4xas.org>
2017-10-06 09:41:35 -0400

    actualizado

--- views/layouts/front.blade.php
+++ views/layouts/front.blade.php
@@ -83,10 +83,10 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-    @stack('css')
     @if(!Auth::check())
-    {{--  <script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');ga('create', 'UA-105362446-1', 'auto');ga('send', 'pageview');</script>  --}}
+    <script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');ga('create', 'UA-105362446-1', 'auto');ga('send', 'pageview');</script>
     @endif
+    @stack('css')
     <style>.footer a{color:#fff;}.btn,a.btn{white-space:inherit;}</style>
 
 </head>
